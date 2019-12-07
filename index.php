203cac93567e77ec5b3a6d7f249b381e1d875347
Vidita <viditavijaykumar.daga@sjsu.edu>
2019-12-07 12:24:44 -0800

    Editing services in index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         <h2>We create <span class="rotating">beautiful graphics, functional websites, working mobile apps</span></h2>
         <div class="actions">
           <a href="#about" class="btn-get-started">Get Strated</a>
-          <a href="#services" class="btn-services">Our Services</a>
+          <a href="#services" class="btn-services">Our Services & Products</a>
         </div>
       </div>
     </div>
@@ -161,40 +161,40 @@
         <div class="col-md-12">
           <h3 class="section-title">Our Services</h3>
           <div class="section-title-divider"></div>
-          <p class="section-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium</p>
+          <p class="section-description">We provide great services & products</p>
         </div>
       </div>
 
       <div class="row">
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-desktop"></i></div>
-          <h4 class="service-title"><a href="">Lorem Ipsum</a></h4>
-          <p class="service-description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+          <h4 class="service-title"><a href="">Fitness Studio</a></h4>
+          <p class="service-description">Provides best fitness services</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-bar-chart"></i></div>
-          <h4 class="service-title"><a href="">Dolor Sitema</a></h4>
-          <p class="service-description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+          <h4 class="service-title"><a href="">Books</a></h4>
+          <p class="service-description">Buy the best books at best price</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-paper-plane"></i></div>
-          <h4 class="service-title"><a href="">Sed ut perspiciatis</a></h4>
-          <p class="service-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+          <h4 class="service-title"><a href="">VR GRAD</a></h4>
+          <p class="service-description">Your one stop shop for all things Virtual Reality and Augmented Reality products sourced from trusted manufacturers</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-photo"></i></div>
-          <h4 class="service-title"><a href="">Magni Dolores</a></h4>
-          <p class="service-description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          <h4 class="service-title"><a href="">PPM Software Solutions</a></h4>
+          <p class="service-description">Offers expertise in building & customizing software projects</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-road"></i></div>
-          <h4 class="service-title"><a href="">Nemo Enim</a></h4>
-          <p class="service-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+          <h4 class="service-title"><a href="">Arcade Motor Services</a></h4>
+          <p class="service-description">We service all kind of automobiles at affordable prices.</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-shopping-bag"></i></div>
-          <h4 class="service-title"><a href="">Eiusmod Tempor</a></h4>
-          <p class="service-description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+          <h4 class="service-title"><a href="">Platinum County Gym</a></h4>
+          <p class="service-description">Top leader in fitness industry</p>
         </div>
       </div>
     </div>
