0eb0237e1239a6745f847647a8443441f7b0f701
Sushant Mathur <sushant.post@gmail.com>
2019-12-09 10:09:47 -0800

    Added login and registration code
    Information taken from the forms and submittied/checked against the database. The password is hashed with sha1, not the most secure but okay for the scope of this project.

---
+++ login.php
@@ -0,0 +1,72 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "users";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	if(!$conn){
+		die("Connection failed!!!!" .mysqli_connect_error());
+	}
+
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		$hash = sha1($password);
+
+		$sql = "SELECT * FROM $dbname WHERE email='$email'";
+
+		if($result = $conn->query($sql)){
+			if(mysqli_num_rows($result) < 1 ){
+				//ERROR OUT
+				echo "No such user";
+			}else{
+				$row = mysqli_fetch_array($result);
+				$pass = $row['Password'];
+				if($pass == $hash){
+					//Passwords match.
+					echo "Passwords match";
+				}else{
+					//No match.
+					echo "Passwords do not match";
+				}
+			}
+		}else{
+			echo "The query did not execute correctly!";
+		}
+	}
+
+
+	$conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<div class="input-group">
+  		<label>Email</label>
+  		<input type="text" name="email" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,84 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "users";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	if(!$conn){
+		die("Connection failed!!!!" .mysqli_connect_error());
+	}
+
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$firstname = $_POST["firstname"];
+		$lastname = $_POST["lastname"];
+		$password = $_POST["password"];
+		$email = $_POST["email"];
+		$homeAddress = $_POST["address"];
+		$homePhone = $_POST["homephone"];
+		$cellPhone = $_POST["cellphone"];
+		$hash = sha1($password);
+	
+		$sql = "INSERT INTO users (FirstName, LastName, Email, Password, HomeAddress
+		,HomePhone, CellPhone) VALUES ('$firstname', '$lastname', '$email' , '$hash' ,'$homeAddress', '$homePhone', '$cellPhone')";
+		if ($conn->query($sql) === TRUE) {
+			echo "New record created successfully";
+		} else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+	
+	$conn->close();
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+	
+  <form method="post" action="register.php">
+      <div class="input-group">
+  	  <label>First Name</label>
+  	  <input type="text" name="firstname" value="first name">
+      </div>
+      <div class="input-group">
+  	  <label>Last Name</label>
+  	  <input type="text" name="lastname" value="last name">
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="email">
+      </div>
+      <div class="input-group">
+  	  <label>Home Address</label>
+  	  <input type="text" name="address" value="address">
+      </div>
+      <div class="input-group">
+  	  <label>Home Phone</label>
+  	  <input type="text" name="homephone" value="home phone">
+      </div>
+      <div class="input-group">
+  	  <label>Cell Phone</label>
+  	  <input type="text" name="cellphone" value="cell phone">
+      </div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a>
+  	</p>
+  </form>
+</body>
+</html>
