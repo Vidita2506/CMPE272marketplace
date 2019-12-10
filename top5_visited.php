9ca2d362d753e83f7dbd56e7871fa757fdfe98e2
Sushant Mathur <sushant.post@gmail.com>
2019-12-09 20:54:21 -0800

    Create top5_visited.php
    Added logic for reading and showing the top 5 visited sites

---
+++ top5_visited.php
@@ -0,0 +1,28 @@
+<?php
+    $servername = "sidharthjayaprakash9360963.ipagemysql.com";
+    $username = "jsidharth";
+    $password = "root123";
+    $dbname = "market_place_1";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if(!$conn){
+        die("Connection failed!!!!" .mysqli_connect_error());
+    }
+
+    $sql_init = "SELECT * FROM product_visits ORDER BY 'count' DESC";
+
+    $result = $conn->query($sql_init);
+    
+    $x=0;
+    while($row = $result->fetch_array()){
+        if($x>=5){
+            break;
+        }
+        echo $row['product_name'];
+        echo $row['count'];
+        $x++;
+    }
+
+    $conn->close();
+
+?>
