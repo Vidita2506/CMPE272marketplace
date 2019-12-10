1f309abb0838a8d7bb918a3413b7054eabce9488
Mitra Nayak <mitragunakara.nayak@sjsu.edu>
2019-12-09 16:19:02 -0800

    index

--- index.php
+++ index.php
@@ -195,13 +195,13 @@
   <!--==========================
   Testimonials Section
   ============================-->
-  <section id="testimonials">
+  <section id="Customer Review ">
     <div class="container wow fadeInUp">
       <div class="row">
         <div class="col-md-12">
           <h3 class="section-title">Testimonials</h3>
           <div class="section-title-divider"></div>
-          <p class="section-description">Erdo lide, nora porodo filece, salvam esse se, quod concedimus ses haec dicturum fuisse</p>
+          <p class="section-description">This is what our loyal customers have to say !!! </p>
         </div>
       </div>
 
@@ -215,7 +215,7 @@
         </div>
         <div class="col-md-9">
           <div class="quote">
-            <b><img src="img/quote_sign_left.png" alt=""></b> Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper. <small><img src="img/quote_sign_right.png" alt=""></small>
+            <b><img src="img/quote_sign_left.png" alt=""></b> I have seldom experienced such an efficient help and support like from you! Thank you so much. You guys are stars!<small><img src="img/quote_sign_right.png" alt=""></small>
           </div>
         </div>
       </div>
@@ -223,7 +223,7 @@
       <div class="row">
         <div class="col-md-9">
           <div class="quote">
-            <b><img src="img/quote_sign_left.png" alt=""></b> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis architecto beatae. <small><img src="img/quote_sign_right.png" alt=""></small>
+            <b><img src="img/quote_sign_left.png" alt=""></b> Many thanks for you kind and efficient service. I have already and will definitely continue to recommend your services to others in the future. Kudos!!! <small><img src="img/quote_sign_right.png" alt=""></small>
           </div>
         </div>
         <div class="col-md-3">
