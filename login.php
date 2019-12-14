471fa4616c8c60069f0194b937e847f16e22e9af
Sushant Mathur <sushant.post@gmail.com>
2019-12-14 13:00:39 -0800

    Tryout for fb login

--- login.php
+++ login.php
@@ -56,6 +56,19 @@
 		}
 	}
 	$conn->close();
+	//session_start();
+	$fb = new Facebook\Facebook([
+		'app_id' => '482620352387135', // Replace {app-id} with your app id
+		'app_secret' => 'bf2828ce3eeec15a2b702d504d7c3c95',
+		'default_graph_version' => 'v3.2',
+	]);
+
+	$helper = $fb->getRedirectLoginHelper();
+
+	$permissions = ['email']; // Optional permissions
+	$loginUrl = $helper->getLoginUrl('https://http://www.jsidharth.com/marketplace/fb-callback.php', $permissions);
+
+	echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 ?>
 
 <!DOCTYPE html>
